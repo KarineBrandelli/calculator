08938c5e24828c6be9d34e84d01961fc9dc15cd2
Karine Brandelli <karinebrandelli@live.com>
2022-12-08 16:51:39 -0300

    refactor: calculate function

--- src/App.js
+++ src/App.js
@@ -1,172 +1,129 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./css/styles.css";
 
 function App() {
-  const [num, setNum] = useState('');
-  const [oldnum, setOldNum] = useState('');
-  const [operator, setOperator] = useState();
-  const [screen, setScreen] = useState('');
-  const expression = [];
+  const [screen, setScreen] = useState("");
 
   function inputNum(e) {
     let input = e.target.value;
     setScreen(screen + input);
-
-    if (expression[1].includes('*', '-', '*', '/')) {
-      setNum(num + input)
-    } else {
-      setNum(oldnum + input)
-    }
-    
-
-    // if (num === 0) {
-    //   setNum(input);
-    // } else {
-    //   setNum(num + input);
-    // }
   };
 
   function clear() {
-    setNum(0);
+    setScreen('');
   };
 
   function porcentage() {
-    setNum(num / 100);
+    setScreen(screen + ' / 100');
   };
 
   function operatorHandler(e) {
     let operatorInput = e.target.value;
-    setOperator(operatorInput);
-    setOldNum(num);
-    setScreen(screen + operatorInput);
-    // setNum(0);
 
+    setScreen(screen + operatorInput);
   };
 
   function calculate() {
-    expression[0] = oldnum;
-    expression[1] = operator;
-    expression[2] = num;
-    console.log(expression)
-    setScreen(eval(expression.join()))
-
-    if (operator === "/") {
-      setNum(parseFloat(oldnum) / parseFloat(num));
-    } else if (operator === "X") {
-      setNum(parseFloat(oldnum) * parseFloat(num));
-    } else if (operator === "-") {
-      setNum(parseFloat(oldnum) - parseFloat(num));
-    } else if (operator === "+") {
-      setNum(parseFloat(oldnum) + parseFloat(num));
-    }
+    setScreen(eval(screen));
   };
 
   return (
     <div className="calculator-container">
-      <div className="result">{screen === '' ? '0' : screen}</div>
+      <div className="result">{screen === "" ? "0" : screen}</div>
       <table>
         <tbody>
           <tr>
             <td>
-              <button onClick={ clear } >
-                C
-              </button>
+              <button onClick={clear}>C</button>
             </td>
             <td>
-              <button onClick={ porcentage } >
-                %
-              </button>
+              <button onClick={ porcentage }>%</button>
             </td>
             <td>
-              <button onClick={ operatorHandler } value="/" >
+              <button onClick={operatorHandler} value="/">
                 /
               </button>
             </td>
             <td>
-              <button onClick={ operatorHandler }  value="*" >
+              <button onClick={operatorHandler} value="*">
                 *
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button onClick={ inputNum } value={7} >
+              <button onClick={inputNum} value={7}>
                 7
               </button>
             </td>
             <td>
-              <button onClick={ inputNum } value={8} >
+              <button onClick={inputNum} value={8}>
                 8
               </button>
             </td>
             <td>
-              <button onClick={ inputNum } value={9} >
+              <button onClick={inputNum} value={9}>
                 9
               </button>
             </td>
             <td>
-              <button onClick={ operatorHandler }  value="-" >
+              <button onClick={operatorHandler} value="-">
                 -
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button onClick={ inputNum } value={4} >
+              <button onClick={inputNum} value={4}>
                 4
               </button>
             </td>
             <td>
-              <button onClick={ inputNum } value={5} >
+              <button onClick={inputNum} value={5}>
                 5
               </button>
             </td>
             <td>
-              <button onClick={ inputNum } value={6} >
+              <button onClick={inputNum} value={6}>
                 6
               </button>
             </td>
             <td>
-              <button onClick={ operatorHandler }  value="+" >
+              <button onClick={operatorHandler} value="+">
                 +
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button onClick={ inputNum } value={1} >
+              <button onClick={inputNum} value={1}>
                 1
               </button>
             </td>
             <td>
-              <button onClick={ inputNum } value={2} >
+              <button onClick={inputNum} value={2}>
                 2
               </button>
             </td>
             <td>
-              <button onClick={ inputNum } value={3} >
+              <button onClick={inputNum} value={3}>
                 3
               </button>
             </td>
             <td rowSpan="2">
-              <button
-                className="equal"
-                onClick={ calculate } >
+              <button className="equal" onClick={calculate}>
                 =
               </button>
             </td>
           </tr>
           <tr>
             <td colSpan="2">
-              <button
-                className="zero"
-                onClick={ inputNum }
-                value={0} >
+              <button className="zero" onClick={inputNum} value={0}>
                 0
               </button>
             </td>
             <td>
-              <button onClick={ inputNum } value="." >
+              <button onClick={inputNum} value=".">
                 .
               </button>
             </td>
@@ -175,6 +132,6 @@ function App() {
       </table>
     </div>
   );
-};
+}
 
 export default App;
