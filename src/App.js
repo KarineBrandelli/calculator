00db37c754d6fc964929d415670d6e9554167645
Karine Brandelli <karinebrandelli@live.com>
2022-12-11 19:03:51 -0300

    build: format code

--- src/App.js
+++ src/App.js
@@ -30,13 +30,13 @@ function App() {
   return (
     <div className="calculator-container">
       <div className="result">
-        {screen === "" ? "0" : screen}
+        { screen === "" ? "0" : screen }
       </div>
       <table>
         <tbody>
           <tr>
             <td>
-              <button onClick={clear}>
+              <button onClick={ clear }>
                 C
               </button>
             </td>
@@ -46,90 +46,90 @@ function App() {
               </button>
             </td>
             <td>
-              <button onClick={operatorHandler} value="/">
+              <button onClick={ operatorHandler } value="/">
                 /
               </button>
             </td>
             <td>
-              <button onClick={operatorHandler} value="*">
+              <button onClick={ operatorHandler } value="*">
                 *
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button onClick={inputNum} value={7}>
+              <button onClick={ inputNum } value={7}>
                 7
               </button>
             </td>
             <td>
-              <button onClick={inputNum} value={8}>
+              <button onClick={ inputNum } value={8}>
                 8
               </button>
             </td>
             <td>
-              <button onClick={inputNum} value={9}>
+              <button onClick={ inputNum } value={9}>
                 9
               </button>
             </td>
             <td>
-              <button onClick={operatorHandler} value="-">
+              <button onClick={ operatorHandler } value="-">
                 -
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button onClick={inputNum} value={4}>
+              <button onClick={ inputNum } value={4}>
                 4
               </button>
             </td>
             <td>
-              <button onClick={inputNum} value={5}>
+              <button onClick={ inputNum } value={5}>
                 5
               </button>
             </td>
             <td>
-              <button onClick={inputNum} value={6}>
+              <button onClick={ inputNum } value={6}>
                 6
               </button>
             </td>
             <td>
-              <button onClick={operatorHandler} value="+">
+              <button onClick={ operatorHandler} value="+">
                 +
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button onClick={inputNum} value={1}>
+              <button onClick={ inputNum } value={1}>
                 1
               </button>
             </td>
             <td>
-              <button onClick={inputNum} value={2}>
+              <button onClick={ inputNum } value={2}>
                 2
               </button>
             </td>
             <td>
-              <button onClick={inputNum} value={3}>
+              <button onClick={ inputNum } value={3}>
                 3
               </button>
             </td>
             <td rowSpan="2">
-              <button className="equal" onClick={calculate}>
+              <button className="equal" onClick={ calculate }>
                 =
               </button>
             </td>
           </tr>
           <tr>
             <td colSpan="2">
-              <button className="zero" onClick={inputNum} value={0}>
+              <button className="zero" onClick={ inputNum } value={0}>
                 0
               </button>
             </td>
             <td>
-              <button onClick={inputNum} value=".">
+              <button onClick={ inputNum } value=".">
                 .
               </button>
             </td>
